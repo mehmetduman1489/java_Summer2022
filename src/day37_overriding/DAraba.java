643b51a6bfa634040b1c320daa09ab61d87b2eda
Mehmet Duman <105608184+mehmetduman1489@users.noreply.github.com>
2022-08-01 23:26:47 +0300

    01.08

--- src/day37_overriding/DAraba.java
+++ src/day37_overriding/DAraba.java
@@ -2,8 +2,12 @@ package day37_overriding;
 
 
 public class DAraba {
-    void yakit(){
+    private void yakit(){
         System.out.println("Tum arabalar yakit kullanir");
+        /*
+         Parent class'dan override edilmesini istemediginiz method'lari
+         private, static veya final yapabilirsiniz
+         */
     }
     void marka(){
         System.out.println("Tum arabalarin markasi vardir");
--- src/day37_overriding/EToyota.java
+++ src/day37_overriding/EToyota.java
@@ -1,12 +1,15 @@
 package day37_overriding;
 
-
 public class EToyota extends DAraba{
-    void marka(){
+    @Override
+    void marka() {
+        /* super.marka();
+          eger hem overridden hem de overridding method'un calismasini istersek
+          ilk satira super.marka() yazabiliriz
+         */
         System.out.println("Markamiz Toyota");
     }
     void motor(){
         System.out.println("Toyota araclar Toyota marka motor kullanir");
     }
 }
-
