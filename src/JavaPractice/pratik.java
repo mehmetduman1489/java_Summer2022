264d43faebf0f27cccbc3395d4c8f5c5534b800f
Mehmet Duman <mehmetduman1489@gmail.com>
2022-08-05 01:37:35 +0300

    05.08

---
+++ src/JavaPractice/pratik.java
@@ -0,0 +1,22 @@
+package JavaPractice;
+
+import java.time.LocalDate;
+
+
+public class pratik {
+    public static void main(String[] args) {
+        LocalDate tarih = LocalDate.now();
+    tarih.
+
+
+
+
+
+
+
+    }
+
+
+
+
+}
