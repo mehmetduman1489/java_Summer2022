047dc7a5d9b219e4d7a7a7fc311bdfd10dd161a2
Mehmet Duman <mehmetduman1489@gmail.com>
2022-08-15 14:41:00 +0300

    15.08

---
+++ src/day100_pratik/l.java
@@ -0,0 +1,15 @@
+package day100_pratik;
+
+public class l {//Herhangi bir döngü kullanmadan 1 den 100e kadar sayıları ekrana yazdıran kodları yazınız
+
+    public static void main(String[] args){
+        //bir tamsayı arrayında ortadaki elemani bulun
+        int [] arr={2,34,55,33,45,67,83,73,63,46,78,99,22,62,67,48,99};
+        System.out.println(arr[(arr.length/2)]);
+
+
+
+
+}
+
+}
