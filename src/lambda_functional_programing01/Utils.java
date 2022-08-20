0dbd2f3a3049f593ad9992bcb4232bb6bcb104b0
Mehmet Duman <mehmetduman1489@gmail.com>
2022-08-20 22:01:53 +0300

    20.08

---
+++ src/lambda_functional_programing01/Utils.java
@@ -0,0 +1,49 @@
+package lambda_functional_programing01;
+
+public class Utils {
+
+    public static void ayniSatirdaBosluklaYazdir(Object obj){
+
+        System.out.print(obj+" ");
+
+    }
+
+    public static boolean ciftElemaniSec(int x){
+
+        return x%2==0;
+
+    }
+
+    public static boolean tekElemaniSec(int x){
+
+        return x%2!=0;
+
+    }
+
+    public static int karesinAl(int x){
+
+        return x*x;
+    }
+
+    public static int kupunuAl(int x){
+
+        return x*x*x;
+    }
+    public static double yarisiniAl(int x){
+
+        return x/2.0;
+
+    }
+
+    public static char sonKarakteriAl(String str){
+
+        return str.charAt(str.length()-1);
+    }
+
+    public static char ilkKarakteriAl(String str){
+
+        return str.charAt(0);
+    }
+
+
+}
