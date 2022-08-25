85af6043501d7430231acbf9b4a76decd6b0aa9a
Mehmet Duman <mehmetduman1489@gmail.com>
2022-08-25 19:43:45 +0300

    25.08

---
+++ src/lambdapractise/Methodlarim.java
@@ -0,0 +1,20 @@
+package lambdapractise;
+
+import java.util.Comparator;
+import java.util.List;
+
+public class Methodlarim {//seed (tohum) methodlar create edildi
+
+    public static void yazdir(Object obj){
+        System.out.print(obj+" ");
+    }
+
+    public static boolean ciftElemaniBul(int x){
+        return  x % 2==0;
+      // 2.yol return x % 2!=1;
+    }
+
+
+
+
+}
---
+++ src/lambdapractise/Runner01.java
@@ -0,0 +1,26 @@
+package lambdapractise;
+
+public class Runner01 extends Lambda01{//inherite birazdan
+
+    public static void main(String[] args) {
+
+       // aHarfleriSilenGerisiniYazdiran(names);
+        System.out.println("**************");
+      //  maxElemanBul(numbers);
+        System.out.println("**************");
+      //  elemanlariTopla(numbers);
+        System.out.println("**************");
+       // elemanlariCarp(numbers);
+        System.out.println("**************");
+        //teklerinKareBuyuktenKucuge(numbers);
+        System.out.println("**************");
+        //sonHarfeGoreTersSira(menu);
+        System.out.println("**************");
+        karkterEnFazla(menu);
+        System.out.println("**************");
+
+
+
+
+    }
+}
